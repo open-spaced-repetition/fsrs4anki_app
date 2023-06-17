30617abd289b1d7b96f57937397b99915218dd75
Ye Junyao <JarrettYe@users.noreply.huggingface.co>
2023-06-17 13:26:05 +0000

    Update app.py

--- app.py
+++ app.py
@@ -73,7 +73,7 @@ def anki_optimizer(file: gr.File, timezone, next_day_starts_at, revlog_start_dat
 
 
 description = """
-# FSRS4Anki Optimizer App - v3.24.5
+# FSRS4Anki Optimizer App - v3.24.6
 Based on the [tutorial](https://medium.com/@JarrettYe/how-to-use-the-next-generation-spaced-repetition-algorithm-fsrs-on-anki-5a591ca562e2) 
 of [Jarrett Ye](https://github.com/L-M-Sherlock). This application can give you personalized anki parameters without having to code.
 
